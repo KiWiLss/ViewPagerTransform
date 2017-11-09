5fbe488b897b58a1700a5b3cc1e99b3fd6aa6742
KiwiLss <m15205622327@163.com>
2017-11-09 12:00:28 +0800

    11-9

--- wave/src/main/java/com/magicsoft/wave/activity/SeekActivity.java
+++ wave/src/main/java/com/magicsoft/wave/activity/SeekActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.magicsoft.wave.R;
 import com.xw.repo.BubbleSeekBar;
 
@@ -35,5 +36,20 @@ public class SeekActivity extends AppCompatActivity {
 
             }
         });
+
+        final LottieAnimationView byId = (LottieAnimationView) findViewById(R.id.animation_view);
+        //加载网络json图片
+       /* JSONObject json = new JSONObject(response.body().string());
+        LottieComposition.Factory.fromJson(getResources(), json, new OnCompositionLoadedListener() {
+            @Override
+            public void onCompositionLoaded(@Nullable LottieComposition composition) {
+                byId.setProgress(0);
+                byId.loop(true);
+                byId.setComposition(composition);
+                byId.playAnimation();
+            }
+        });*/
+
+
     }
 }
