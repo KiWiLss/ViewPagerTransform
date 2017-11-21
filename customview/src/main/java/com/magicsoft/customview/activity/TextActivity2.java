f56ded2b227b5e4a20fa4b21abdaef3b0141cb2f
KiwiLss <m15205622327@163.com>
2017-11-21 16:58:06 +0800

    11-21

--- customview/src/main/java/com/magicsoft/customview/activity/TextActivity2.java
+++ customview/src/main/java/com/magicsoft/customview/activity/TextActivity2.java
@@ -1,15 +1,25 @@
 package com.magicsoft.customview.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 import com.magicsoft.customview.R;
 import com.magicsoft.customview.model.AnswerRange;
+import com.magicsoft.customview.utils.AddressPickTask;
 import com.magicsoft.customview.widget.FillBlankView;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import cn.qqtheme.framework.entity.City;
+import cn.qqtheme.framework.entity.County;
+import cn.qqtheme.framework.entity.Province;
+import cn.qqtheme.framework.picker.DatePicker;
+import cn.qqtheme.framework.util.ConvertUtils;
 
 /**
  * Created by 刘少帅 on 2017/11/10
@@ -17,6 +27,7 @@ import java.util.List;
 
 public class TextActivity2 extends AppCompatActivity {
 
+    public static final String TAG = "MMM";
     private FillBlankView fillBlankView;
 
     @Override
@@ -40,5 +51,72 @@ public class TextActivity2 extends AppCompatActivity {
         rangeList.add(new AnswerRange(38, 46));
 
         fillBlankView.setData(content, rangeList);
+    }
+
+    public void cityChoice(View view) {
+        //年月日
+        final DatePicker picker = new DatePicker(this);
+        picker.setCanceledOnTouchOutside(true);
+        picker.setUseWeight(true);
+        picker.setTopPadding(ConvertUtils.toPx(this, 10));
+        picker.setRangeEnd(2111, 1, 11);
+        picker.setRangeStart(2016, 8, 29);
+        picker.setSelectedItem(2050, 10, 14);
+        picker.setResetWhileWheel(false);
+        picker.setOnDatePickListener(new DatePicker.OnYearMonthDayPickListener() {
+            @Override
+            public void onDatePicked(String year, String month, String day) {
+                Log.e(TAG, "onDatePicked: "+(year + "-" + month + "-" + day) );
+            }
+        });
+        picker.setOnWheelListener(new DatePicker.OnWheelListener() {
+            @Override
+            public void onYearWheeled(int index, String year) {
+                picker.setTitleText(year + "-" + picker.getSelectedMonth() + "-" + picker.getSelectedDay());
+            }
+
+            @Override
+            public void onMonthWheeled(int index, String month) {
+                picker.setTitleText(picker.getSelectedYear() + "-" + month + "-" + picker.getSelectedDay());
+            }
+
+            @Override
+            public void onDayWheeled(int index, String day) {
+                picker.setTitleText(picker.getSelectedYear() + "-" + picker.getSelectedMonth() + "-" + day);
+            }
+        });
+        picker.show();
+    }
+
+    //接收选择器选中的结果：
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+    }
+
+    public void cityChoiceWheel(View view) {
+        AddressPickTask task = new AddressPickTask(this);
+        task.setHideProvince(false);
+        task.setHideCounty(false);
+
+
+        task.setCallback(new AddressPickTask.Callback() {
+            @Override
+            public void onAddressInitFailed() {
+                Log.e(TAG, "onAddressInitFailed: " );
+            }
+
+            @Override
+            public void onAddressPicked(Province province, City city, County county) {
+                if (county == null) {
+                    Log.e(TAG, "onAddressPicked: "+(province.getAreaName() + city.getAreaName()));
+                } else {
+                    Log.e(TAG, "onAddressPicked: "+(province.getAreaName() + city.getAreaName() + county.getAreaName()));
+                }
+            }
+        });
+        task.execute("贵州", "毕节", "纳雍");
     }
 }
