537ae764754a0087bef4ded9ba637567df8266e1
KiwiLss <m15205622327@163.com>
2017-11-23 11:48:33 +0800

    11-23

--- vector/src/main/java/com/magicsoft/vector/MainActivity.java
+++ vector/src/main/java/com/magicsoft/vector/MainActivity.java
@@ -1,11 +1,15 @@
 package com.magicsoft.vector;
 
+import android.content.Intent;
 import android.graphics.drawable.Animatable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.view.View;
 import android.widget.ImageView;
+
+import com.magicsoft.vector.activity.BottomBehaviorActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,5 +35,19 @@ public class MainActivity extends AppCompatActivity {
         if (drawableColor instanceof Animatable) {
             ((Animatable) drawableColor).start();
         }
+    }
+
+    public void dayListener(View view) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        recreate();
+    }
+
+    public void nightListener(View view) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        recreate();
+    }
+
+    public void jumpListener(View view) {
+        startActivity(new Intent(this, BottomBehaviorActivity.class));
     }
 }
---
+++ vector/src/main/java/com/magicsoft/vector/activity/BottomBehaviorActivity.java
@@ -0,0 +1,72 @@
+package com.magicsoft.vector.activity;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.magicsoft.vector.R;
+
+public class BottomBehaviorActivity extends AppCompatActivity {
+
+    public static final String TAG = "MMM";
+    private LinearLayout mlinearlayout;
+    BottomSheetBehavior behavior;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_behavior);
+
+        mlinearlayout = (LinearLayout) findViewById(R.id.mlinearlayout);
+
+    behavior = BottomSheetBehavior.from(mlinearlayout);
+        behavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+
+        mlinearlayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            }
+        });
+
+        behavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(View bottomSheet, int newState) {
+                Log.e(TAG, "onStateChanged: "+bottomSheet+"||"+newState );
+            }
+            @Override
+            public void onSlide(View bottomSheet, float slideOffset) {
+                Log.e(TAG, "onSlide: "+bottomSheet+"||"+slideOffset );
+            }
+        });
+
+
+    }
+
+    public void showBottom(View view) {
+        behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+    }
+
+    public void showDialog(View view) {
+
+
+        final BottomSheetDialog dialog = new BottomSheetDialog(this);
+        View pwView = LayoutInflater.from(this).inflate(R.layout.bottomdialog_layout, null);
+        LinearLayout linearLayout = (LinearLayout)pwView.findViewById(R.id.dialog_layout);
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(BottomBehaviorActivity.this, "click", Toast.LENGTH_SHORT).show();
+            }
+        });
+        dialog.setContentView(pwView);
+        dialog.show();
+    }
+}
