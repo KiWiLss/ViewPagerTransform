3259ff581a7a1a78f9673c7c58364cb5de8f1c32
KiwiLss <m15205622327@163.com>
2017-12-18 11:05:08 +0800

    dilaog

--- recycler/src/main/java/com/magicsoft/recycler/activity/TwoActivity.java
+++ recycler/src/main/java/com/magicsoft/recycler/activity/TwoActivity.java
@@ -1,8 +1,18 @@
 package com.magicsoft.recycler.activity;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
+import android.view.View;
 
 import com.magicsoft.recycler.R;
 
@@ -34,5 +44,64 @@ public class TwoActivity extends AppCompatActivity {
 
 
 
+    }
+
+    public void goole(View view) {
+        String title="原生样式标题";
+        //改变标题
+        SpannableStringBuilder Builder = new
+                SpannableStringBuilder(title);
+
+        StyleSpan spanState1 = new
+                StyleSpan(Typeface.BOLD);
+        Builder.setSpan(spanState1,0,title.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+
+        Builder.setSpan(new ForegroundColorSpan(Color.YELLOW),0,3,Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder
+                .setTitle(Builder).setIcon(R.drawable.skin_left_menu_icon)
+                .setMessage("这是一个原生样式的对话框")
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+        //单独修改按钮的颜色等,要在show方法后执行
+        alertDialog.getButton(DialogInterface.BUTTON_NEGATIVE).setTextColor(ContextCompat.getColor(this,R.color.colorPrimaryDark));
+        alertDialog.getButton(DialogInterface.BUTTON_POSITIVE).setTextColor(ContextCompat.getColor(this,R.color.colorPrimaryDark));
+
+
+
+    }
+
+    public void themeOne(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this,R.style.MyDialogTheme);
+        builder
+                .setTitle("原生样式标题").setIcon(R.drawable.skin_left_menu_icon)
+                .setMessage("这是一个原生样式的对话框")
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        //alertDialog.getButton(alertDialog.BUTTON_NEGATIVE).setTextColor(ContextCompat.getColor(this,R.color.colorAccent));
+        alertDialog.show();
     }
 }
